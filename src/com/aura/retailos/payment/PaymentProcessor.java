b52268e3b07f76819bcc6ac9486d7fd88480b48f
priya <plpatel078@gmail.com>
2026-04-11 12:51:38 +0530

    Add payment system stubs - Adapter pattern - Priya

--- src/com/aura/retailos/payment/PaymentProcessor.java
+++ src/com/aura/retailos/payment/PaymentProcessor.java
@@ -4,8 +4,9 @@ package com.aura.retailos.payment;
 
 public interface PaymentProcessor {
 
-    // Processes a payment of the given amount using the provided payment details
-    boolean processPayment(double amount, String paymentDetails);
+    // Processes a payment of the given amount using the provided payment details;
+    // returns true if the payment was accepted by the provider
+    boolean processPayment(double amount, String details);
 
     // Returns the name of the payment provider this processor adapts
     String getProviderName();
