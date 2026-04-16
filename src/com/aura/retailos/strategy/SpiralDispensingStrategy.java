f3b6e5d88840a917fadb9d0c21ff673e9a60b8df
GaurangRahani <gaurangrahani792@gmail.com>
2026-04-16 15:51:27 +0530

    added SpiralDispensingStrategy using spiral pattern

--- src/com/aura/retailos/strategy/SpiralDispensingStrategy.java
+++ src/com/aura/retailos/strategy/SpiralDispensingStrategy.java
@@ -7,12 +7,13 @@ public class SpiralDispensingStrategy implements DispensingStrategy {
     // Dispenses a product using the spiral mechanism strategy
     @Override
     public boolean dispense(String product) {
-        throw new UnsupportedOperationException("To be implemented");
+        System.out.println("[STRATEGY-SPIRAL] Spiral dispensing: " + product);
+        return true;
     }
 
     // Returns the name of this strategy
     @Override
     public String getStrategyName() {
-        throw new UnsupportedOperationException("To be implemented");
+        return "SpiralDispensingStrategy";
     }
 }
