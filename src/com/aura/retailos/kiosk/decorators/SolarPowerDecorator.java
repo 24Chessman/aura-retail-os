e48b3ec0592e51ec7403ec2be7c2a2caf3f8045b
GaurangRahani <gaurangrahani792@gmail.com>
2026-04-18 11:08:24 +0530

    implemented SolarPowerDecorator with solar wattage and battery reserve

--- src/com/aura/retailos/kiosk/decorators/SolarPowerDecorator.java
+++ src/com/aura/retailos/kiosk/decorators/SolarPowerDecorator.java
@@ -3,41 +3,44 @@
 package com.aura.retailos.kiosk.decorators;
 
 import com.aura.retailos.kiosk.Kiosk;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SolarPowerDecorator extends KioskDecorator {
 
     // Current solar energy input in watts
-    private double solarInput;
+    private double solarInput = 120.5;
 
     // Current battery reserve level as a percentage
-    private double batteryReserve;
+    private double batteryReserve = 85.0;
 
     // Constructs a SolarPowerDecorator wrapping the given kiosk
     public SolarPowerDecorator(Kiosk kiosk) {
         super(kiosk);
-        throw new UnsupportedOperationException("To be implemented");
+        System.out.println("[DECORATOR] SolarPowerModule attached to " + kiosk.getKioskId());
     }
 
     // Returns status combined with solar power module readings
     @Override
     public String getStatus() {
-        throw new UnsupportedOperationException("To be implemented");
+        return super.getStatus() + " | Solar: " + solarInput + "W | Battery: " + batteryReserve + "%";
     }
 
     // Returns the current solar energy input in watts
     public double getSolarInput() {
-        throw new UnsupportedOperationException("To be implemented");
+        return solarInput;
     }
 
     // Returns the current battery reserve percentage
     public double getBatteryReserve() {
-        throw new UnsupportedOperationException("To be implemented");
+        return batteryReserve;
     }
 
     // Returns capabilities including solar power support
     @Override
     public List<String> getCapabilities() {
-        throw new UnsupportedOperationException("To be implemented");
+        List<String> caps = new ArrayList<>(super.getCapabilities());
+        caps.add("SolarPowerMonitoring");
+        return caps;
     }
 }
